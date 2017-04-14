452d8a7abde98593ce7d16be1095a1517b4689f2
sethvincent <sethvincent@gmail.com>
2017-04-14 16:55:04 -0400

    use correct deviceId

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var loginButton = document.getElementById('login')
 
 var alexa = Alexa({
   debug: true,
-  deviceId: 'alexa-satellite-dev-2',
+  deviceId: 'alexa_satellite_dev_2',
   deviceSerialNumber: 1,
   clientId: 'amzn1.application-oa2-client.93f8a2df954f4039866e7e06f5652f49',
   redirectUri: `https://${window.location.host}/auth`
