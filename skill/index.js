f321698201835d1dc454b73c58e5061f3eab0244
sethvincent <sethvincent@gmail.com>
2018-01-09 17:36:45 -0500

    update ec2 url

--- skill/index.js
+++ skill/index.js
@@ -143,7 +143,7 @@ function getImageResponse (intentRequest, session, callback) {
       apiUrl += '&satellite_name=landsat';
     }
 
-    var tilerUrl = 'http://ec2-54-242-26-167.compute-1.amazonaws.com/image/';
+    var tilerUrl = 'http://ec2-52-38-234-42.compute-1.amazonaws.com/image/';
 
     requestImage(apiUrl, function (err, body) {
       options = {
